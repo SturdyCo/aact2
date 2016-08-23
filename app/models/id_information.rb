53b01bf3dfaa552931bb5e6889864b16647e7c25
tibbs001@duke.edu <tibbs001@duke.edu>
2016-08-23 16:13:54 -0400

    AACT-179:  Rename table id_informations to id_information.  Also, we weren't creating a row for the NCT alias

--- app/models/id_information.rb
+++ app/models/id_information.rb
@@ -1,22 +1,28 @@
 class IdInformation < StudyRelationship
+  self.table_name = "id_information"
   def self.top_level_label
     '//id_info'
   end
 
   def self.create_all_from(opts)
-    id_informations =  org_study_ids(opts) + secondary_ids(opts)
+    id_informations =  org_study_ids(opts) + secondary_ids(opts) + aliases(opts)
     IdInformation.import(id_informations)
   end
 
   def self.org_study_ids(opts)
     opts[:xml].xpath("//org_study_id").collect{|xml|
-      IdInformation.new.create_from({:xml=>xml, :id_type=>'org_study_id', :id_value=>xml.text.strip, :nct_id=>(opts[:nct_id] || opts[:nct_alias]) }) }
+      IdInformation.new.create_from({:xml=>xml, :id_type=>'org_study_id', :id_value=>xml.text.strip, :nct_id=>(opts[:nct_id]) }) }
 
   end
 
   def self.secondary_ids(opts)
     opts[:xml].xpath("//secondary_id").collect{|xml|
-      IdInformation.new.create_from({:xml=>xml, :id_type=>'secondary_id', :id_value=>xml.text.strip, :nct_id=>(opts[:nct_id] || opts[:nct_alias]) }) }
+      IdInformation.new.create_from({:xml=>xml, :id_type=>'secondary_id', :id_value=>xml.text.strip, :nct_id=>(opts[:nct_id]) }) }
+  end
+
+  def self.aliases(opts)
+    opts[:xml].xpath("//nct_alias").collect{|xml|
+      IdInformation.new.create_from({:xml=>xml, :id_type=>'nct_alias', :id_value=>xml.text.strip, :nct_id=>(opts[:nct_id]) }) }
   end
 
   def create_from(opts={})
---
+++ db/migrate/20160823195243_aact_179_rename_id_information.rb
@@ -0,0 +1,11 @@
+class Aact179RenameIdInformation < ActiveRecord::Migration
+  def change
+    drop_table :id_informations
+    create_table :id_information do |t|
+      t.string :nct_id
+      t.string :id_type
+      t.string :id_value
+    end
+  end
+end
+
